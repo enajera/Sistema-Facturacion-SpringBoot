8f4894c354b07974a90758ecf3b3cccf154e3d55
Vinn <elvin7n@gmail.com>
2021-01-27 12:11:40 -0600

    Creando aplicacion SpringBoot Sistema de Facturas

---
+++ src/test/java/com/vinn/springboot/app/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinn.springboot.app;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
